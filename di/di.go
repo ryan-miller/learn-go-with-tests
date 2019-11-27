493bced6fda581a576d8bb5f1bbdda3eac2eb838
Ryan Miller <rmiller@fastly.com>
2019-11-27 11:30:04 -0700

    direct injection

---
+++ di/di.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "ryan")
+}
+
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreetHandler))
+}
